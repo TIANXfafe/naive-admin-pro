0c0dca0e748303a2522655df3f1a5ffd465ab9ea
tian <1830015699@qq.com>
2023-09-12 11:52:59 +0800

    ✨登录页面及用户共享状态封装

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath } from 'url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -10,51 +10,64 @@ import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
 const baseSrc = fileURLToPath(new URL('./src', import.meta.url))
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    __VUE_I18N_FULL_INSTALL__: false,
-    __VUE_I18N_LEGACY_API__: false,
-  },
-  plugins: [
-    ReactivityTransform(),
-    vue({
-      reactivityTransform: true,
-    }),
-    AutoImport({
-      imports: [
-        'vue',
-        'vue/macros',
-        'vue-router',
-        'vue-i18n',
-        '@vueuse/core',
-        'pinia',
-        {
-          'naive-ui': [
-            'useDialog',
-            'useMessage',
-            'useNotification',
-            'useLoadingBar',
-          ],
-        },
-      ],
-      dts: 'types/auto-imports.d.ts',
-      dirs: [
-        'src/stores',
-        'src/composables',
-      ],
-    }),
-    Components({
-      resolvers: [NaiveUiResolver()],
-      dts: 'types/components.d.ts',
-      dirs: [
-        'src/components',
-      ],
-    }),
-    Unocss(),
-  ],
-  resolve: {
-    alias: {
-      '@': baseSrc,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    define: {
+      __VUE_I18N_FULL_INSTALL__: false,
+      __VUE_I18N_LEGACY_API__: false,
     },
-  },
+    plugins: [
+      ReactivityTransform(),
+      vue({
+        reactivityTransform: true,
+      }),
+      AutoImport({
+        imports: [
+          'vue',
+          'vue/macros',
+          'vue-router',
+          'vue-i18n',
+          '@vueuse/core',
+          'pinia',
+          {
+            'naive-ui': [
+              'useDialog',
+              'useMessage',
+              'useNotification',
+              'useLoadingBar',
+            ],
+          },
+        ],
+        dts: 'types/auto-imports.d.ts',
+        dirs: [
+          'src/stores',
+          'src/composables',
+        ],
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()],
+        dts: 'types/components.d.ts',
+        dirs: [
+          'src/components',
+        ],
+      }),
+      Unocss(),
+    ],
+    resolve: {
+      alias: {
+        '@': baseSrc,
+      },
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_BASE_URL,
+          changeOrigin: true,
+          ws: false,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
